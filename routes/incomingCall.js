d02c695d4405e5e9f730a884e37b5a4631e831d9
Elishama-Pintosevich <127098904+Elishama-Pintosevich@users.noreply.github.com>
2023-10-31 13:16:31 +0200

    Add files via upload

--- routes/incomingCall.js
+++ routes/incomingCall.js
@@ -6,7 +6,7 @@ router.get("/", async(req,res) => {
   res.json({msg:"incoming call work!"})
 })
 
-router.post("/voice", async(req,res)=>{
+router.post("/voice1", async(req,res)=>{
     const twiml = new VoiceResponse();
     // twiml.play("https://call-project.cyclic.app/Rev.mp3");
 
@@ -35,16 +35,59 @@ router.post("/voice", async(req,res)=>{
     } else {
       gather();
     }
-    // res.writeHead(200, { 'Content-Type': 'text/xml' });
-    // res.end(twiml.toString());
-    // res.end(twiml.toString());
-    // res.json({msg:twiml.toString()})
     res.type('text/xml');
     res.send(twiml.toString());
 
 })
-router.get("/test", async(req,res)=>{
-    res.json({msg:"test work2!"})
+
+router.post("/voice", async(req,res)=>{
+  const twiml = new VoiceResponse();
+
+  const gather = twiml.gather({
+    numDigits: 1,
+    action:'https://call-project.cyclic.app/incomingCall/gather',
+    method: 'POST'
+  });
+  
+  gather.play("https://call-project.cyclic.app/Rev.mp3");
+  gather.say('For sales, press 1. For support, press 2.');
+
+  twiml.redirect({
+    method: 'POST'
+}, 'https://call-project.cyclic.app/incomingCall/voice');
+
+  res.type('text/xml');
+  res.send(twiml.toString());
 })
+
+router.post("/gather", async(req,res)=>{
+  const twiml = new VoiceResponse();
+  
+  if (req.body.Digits) {
+    switch (req.body.Digits) {
+      case '1':
+        twiml.say('You selected sales. Good for you!');
+        break;
+      case '2':
+        twiml.say('You need support. We will help!');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.");
+        twiml.pause();
+        twiml.redirect({
+          method: 'POST'
+      }, 'https://call-project.cyclic.app/incomingCall/voice');
+        break;
+    }
+  } else {
+    twiml.redirect({
+      method: 'POST'
+  }, 'https://call-project.cyclic.app/incomingCall/voice');
+  }
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+})
+
 
 module.exports = router;
