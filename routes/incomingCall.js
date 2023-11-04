996e2b52fb4396573df4bb3b9e6d2b12d462b145
Elishama-Pintosevich <127098904+Elishama-Pintosevich@users.noreply.github.com>
2023-11-04 23:14:25 +0200

    Add files via upload

--- routes/incomingCall.js
+++ routes/incomingCall.js
@@ -81,7 +81,8 @@ router.post("/gather", async(req,res)=>{
 
         break;
       case '2':
-        twiml.say({language: 'he-IL', voice: 'Google.he-IL-Standard-B'},'הִגעת למחלקת התרומות, ליצירת קשר אנא חייג 0548543491, תודה');
+        twiml.say({language: 'he-IL', voice: 'Google.he-IL-Standard-B'},'הִגעת למחלקת התרומות');
+        twiml.say({language: 'he-IL', voice: 'Google.he-IL-Standard-B', loop:3},'.ליצירת קשר אנא חייג למספר 0 5 4 8 5 4 3 4 9 1, תודה')
         break;
       default:
         twiml.say({language: 'he-IL', voice: 'Google.he-IL-Standard-B'},"סליחה, המספר איננו מזוהה");
